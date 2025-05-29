b71b5532ebe73fe3fb37ac2ce9c0f47bac0e913e
jelg <perpetualmoti0n@outlook.com>
2025-05-29 12:52:08 -0600

    Porcentage and decimal validation modified

--- backend/products.py
+++ backend/products.py
@@ -4,10 +4,12 @@ class Product:
         self.price = price
 
     def get_discounted_price(self, discount_rate=0):
-        return round(self.price * (1 - discount_rate), 2) #2 decimal added
+        result = self.price * (1 - discount_rate)
+        return float("{:.2f}".format(result)) #2 decimal added
 
 # 20% discount
 class DigitalProduct(Product):
     def get_discounted_price(self, discount_rate=0):
         base_price = super().get_discounted_price(discount_rate)
-        return round(base_price * 0.80, 2) #2 decimal added
+        result = base_price * 0.80
+        return float("{:.2f}".format(result)) #2 decimal added
